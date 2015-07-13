8de64a6df08f1ee4bfa234c0a5d70440278b4300
Laura Fowler <laura@ucar.edu>
2015-07-13 12:13:45 -0600

    * Added the "mpas" pre-compiler option so that the "kind" options can be used within   the MPAS framework.

--- src/core_atmosphere/physics_nggps/physics_nggps_src/machine.f
+++ src/core_atmosphere/physics_nggps/physics_nggps_src/machine.f
@@ -1,7 +1,38 @@
+#if defined(mpas)
+
+! Copyright (c) 2013,  Los Alamos National Security, LLC (LANS) (LA-CC-13-047)
+! and the University Corporation for Atmospheric Research (UCAR).
+!
+! Unless noted otherwise source code is licensed under the BSD license.
+! Additional copyright and license information can be found in the LICENSE file
+! distributed with this code, or at http://mpas-dev.github.com/license.html
+!
+!==================================================================================================
+ module machine
+
+ implicit none
+ save
+
+ integer, parameter :: kind_io4  = selected_real_kind(6),      &
+                       kind_io8  = selected_real_kind(12),     &
+                       kind_ior  = selected_real_kind(12),     &
+                       kind_evod = selected_real_kind(12),     &
+                       kind_dbl_prec = selected_real_kind(12), &
+                       kind_rad  = selected_real_kind(12),     & ! maps to 64-bit real
+                       kind_phys = selected_real_kind(12),     & ! maps to 64-bit real
+                       kind_REAL = selected_real_kind(12),     & ! used in cmp_comm
+                       kind_INTEGER = selected_int_kind(9)
+
+!==================================================================================================
+ end module machine
+!==================================================================================================
+
+#else
+
       MODULE MACHINE
 
       IMPLICIT NONE
-      
+
 #ifndef SINGLE_PREC
       integer, parameter :: kind_io4  = 4, kind_io8  = 8 , kind_ior = 8 &
      &,                     kind_evod = 8, kind_dbl_prec = 8            &
@@ -29,3 +60,6 @@
       real(kind=kind_evod), parameter :: grib_undef = 9.99e20     ! grib undefine value
 !
       END MODULE MACHINE
+
+#endif
+
